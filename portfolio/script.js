2f73121a582a7be9a1bb916dbc069be71858eead
nigerade <64471143+nigerade@users.noreply.github.com>
2020-07-30 20:14:09 -0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,18 @@
+$('nav a').click(function (e) {
+
+    e.preventDefault();
+
+    var id = $(this).attr('href');
+
+        targetOffset = $(id).offset().top,
+
+        menuHeight = $('nav').innerHeight();
+
+    console.log(menuHeight);
+
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 900);
+
+})
+
